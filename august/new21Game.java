fef8ae6f94c79a8853a69bd713badfa05dbda5c7
kushalbug <kj814766@gmail.com>
2025-08-17 14:11:41 +0530

    day-7 daily log

---
+++ august/new21Game.java
@@ -0,0 +1,26 @@
+//day-7 
+
+class Solution {
+    public double new21Game(int n, int k, int maxPts) {
+        if (k == 0 || n >= k - 1 + maxPts) return 1.0;
+
+        double[] dp = new double[n + 1];
+        dp[0] = 1.0;
+        double windowSum = 1.0, result = 0.0;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = windowSum / maxPts;
+
+            if (i < k) {
+                windowSum += dp[i]; // still drawing
+            } else {
+                result += dp[i]; // stopped drawing
+            }
+
+            if (i - maxPts >= 0) {
+                windowSum -= dp[i - maxPts];
+            }
+        }
+        return result;
+    }
+}
