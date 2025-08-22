063f81890745a546998a651ef151e89ad9b772e4
kushalbug <kj814766@gmail.com>
2025-08-22 23:23:10 +0530

    new changes

--- august/minAreaRect.java
+++ august/minAreaRect.java
@@ -1,7 +1,7 @@
 //day-12
 
-class Solution {
-    public int minAreaRect(int[][] grid) {
+class Solution { 
+    public int minimumArea(int[][] grid) {
         int m = grid.length, n = grid[0].length;
 
         int minRow = m, maxRow = -1, minCol = n, maxCol = -1;
@@ -18,7 +18,10 @@ class Solution {
             }
         }
 
-        // Step 2: Compute area
+        // Step 2: If no 1's exist, return 0
+        if (maxRow == -1) return 0;
+
+        // Step 3: Compute area
         int height = maxRow - minRow + 1;
         int width = maxCol - minCol + 1;
 
