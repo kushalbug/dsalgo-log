91851aca3de43a3d64dcfe47bbdf85be126cb023
kushalbug <kj814766@gmail.com>
2025-08-25 10:35:36 +0530

    day-15 of daily log

---
+++ august/findDiagonalOrder.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int[] result = new int[m * n];
+        
+        int row = 0, col = 0, idx = 0;
+        boolean up = true;
+
+        while (idx < m * n) {
+            result[idx++] = mat[row][col];
+
+            if (up) { // moving up-right
+                if (col == n - 1) { 
+                    row++; 
+                    up = false; 
+                } else if (row == 0) { 
+                    col++; 
+                    up = false; 
+                } else { 
+                    row--; 
+                    col++; 
+                }
+            } else { // moving down-left
+                if (row == m - 1) { 
+                    col++; 
+                    up = true; 
+                } else if (col == 0) { 
+                    row++; 
+                    up = true; 
+                } else { 
+                    row++; 
+                    col--; 
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ august/longestSubarray.java
@@ -0,0 +1,25 @@
+//day-14
+
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int left = 0, zeros = 0, maxLen = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) {
+                zeros++;
+            }
+
+            while (zeros > 1) {
+                if (nums[left] == 0) {
+                    zeros--;
+                }
+                left++;
+            }
+
+            // window length minus one (because we must delete one element)
+            maxLen = Math.max(maxLen, right - left);
+        }
+
+        return maxLen;
+    }
+}
