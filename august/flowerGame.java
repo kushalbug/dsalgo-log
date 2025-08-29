386126b8abf9bc64c63a288dd4108112bdd64d91
kushalbug <kj814766@gmail.com>
2025-08-29 11:48:59 +0530

    day-19 of daily log

---
+++ august/flowerGame.java
@@ -0,0 +1,12 @@
+//day-19 of daily log 
+
+class Solution {
+    public long flowerGame(int n, int m) {
+        long oddX = (n + 1) / 2;
+        long evenX = n / 2;
+        long oddY = (m + 1) / 2;
+        long evenY = m / 2;
+
+        return oddX * evenY + evenX * oddY;
+    }
+}
