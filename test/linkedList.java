338ab08680608dae3a4802802e3432453dd20066
kushalbug <kj814766@gmail.com>
2025-09-20 00:19:03 +0530

    reverse a linked list

--- test/linkedList.java
+++ test/linkedList.java
@@ -1,3 +1,30 @@
-public linkedList {
-    
+public class linkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void reverse(){
+        Node prev = null;
+        Node curr = head;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+    public static void main(String args[]){
+
+    }
 }
