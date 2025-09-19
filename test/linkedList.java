a6d8a716589e2c982f6c327829c3d0f511f5c6a4
kushalbug <kj814766@gmail.com>
2025-09-20 00:47:44 +0530

    detect a cycle in LL

--- test/linkedList.java
+++ test/linkedList.java
@@ -10,6 +10,7 @@ public class linkedList {
     }
     public static Node head;
     public static Node tail;
+    public static int size;
 
     public void reverse(){
         Node prev = null;
@@ -24,6 +25,49 @@ public class linkedList {
         }
         head = prev;
     }
+
+    public void deleteNthFromEnd(int n){
+        // calculate size
+        int sz = 0;
+        Node temp = head;
+        while(temp != null){
+            temp = temp.next;
+            sz++;
+        }
+
+        if(n == sz){
+            head = head.next; // remove first
+            return;
+        }
+
+        //sz-n 
+        int i = 1;
+        int iToFind = sz - n;
+        Node prev = head;
+        while(i < iToFind){
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next; // remove nth node
+        return;
+    }
+
+    public boolean isCycle(){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next; // +1
+            fast = fast.next.next; // +2
+
+            if(slow == fast){
+                return true; // cycle exists
+            }
+        }
+        return false; // cycle not exists
+    }
+    
+    
     public static void main(String args[]){
 
     }
