5d6b59075774e4c94e79c691eb7082d83c898f40
kushalbug <kj814766@gmail.com>
2025-09-29 16:39:19 +0530

    reverse a stack

--- test/StackA.java
+++ test/StackA.java
@@ -1,17 +1,40 @@
 import java.util.*;
 
 public class StackA {
-    public static void main(String args[]){
-        Stack<Integer> s = new Stack<>();
+    public static void pushAtBottom(Stack<Integer> s, int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
 
-        s.push(1);
-        s.push(2);
-        s.push(3);
-        s.push(4);
-        s.push(5);
-
+    public static void printStack(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
         while(!s.isEmpty()){
             System.out.println(s.pop());
         }
+    }
+
+    public static void reverseStack(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top = s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
+    }
+    public static void main(String args[]){
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        printStack(s); 
+        
     }
 }
