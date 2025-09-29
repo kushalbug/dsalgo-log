916d9dae312e74fa3e17495fd757d2bc7c68d2be
kushalbug <kj814766@gmail.com>
2025-09-29 15:16:08 +0530

    reverse a string

--- test/StackB.java
+++ test/StackB.java
@@ -11,18 +11,34 @@ public class StackB {
         stack.push(top);
     }
 
-    public static void reverseString(String str){
-        
+    public static String reverseString(String str){
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while(idx < str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+
+        StringBuilder result = new StringBuilder("");
+        while(!s.isEmpty()){    
+            char curr = s.pop();
+            result.append(curr);
+        }
+        return result.toString();
     }
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
+        // Stack<Integer> stack = new Stack<>();
+        // stack.push(1);
+        // stack.push(2);
+        // stack.push(3);
         
-        pushAtBottom(stack , 4);
-        while(!stack.isEmpty()){
-            System.out.println(stack.pop());
-        }
+        // pushAtBottom(stack , 4);
+        // while(!stack.isEmpty()){
+        //     System.out.println(stack.pop());
+        // }
+
+        String str = "abcd";
+        String result = reverseString(str);
+        System.out.println(result);
     }
 }
