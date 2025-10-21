fb1c04fa2b569593dfe3ab261dd9bc5e75c29a0d
kushalbug <kj814766@gmail.com>
2025-10-21 11:56:45 +0530

    first non repeating letter

--- test/Queue/Classroom.java
+++ test/Queue/Classroom.java
@@ -1,30 +1,34 @@
+//first non repeating letter in a stream of characters
+
 import java.util.*;
 
 public class Classroom {
     public static void printNonRepeating(String str){
-        int freq[] = new int[26]; //'a' - 'z'
+        int freq[] = new int[26];
         Queue<Character> q = new LinkedList<>();
 
-        for(int i=0; i<str.length(); i++){
+        for(int i=0 ; i<str.length() ; i++){
             char ch = str.charAt(i);
             q.add(ch);
-            freq[ch - 'a']++;
+            freq[ch-'a']++;
 
             while(!q.isEmpty() && freq[q.peek() - 'a'] > 1){
-                q.remove();
+            q.remove();
             }
 
-            if(q.isEmpty()) {
+            
+            if(q.isEmpty()){
                 System.out.print(-1 + " ");
             } else {
                 System.out.print(q.peek() + " ");
             }
-        }
-        System.out.println();
     }
-    public static void main(String args[]){
-        String str = "aabccxb";
-        printNonRepeating(str);
 
+
+    }
+    public static void main(String[] args) {
+         String str = "aabccxb";
+
+         printNonRepeating(str);
     }
 }
