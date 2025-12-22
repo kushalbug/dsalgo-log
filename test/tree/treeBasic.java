d3a3f06fad84736a00799c6e9ae1dd0c9fd3f650
kushalbug <kj814766@gmail.com>
2025-12-23 00:00:05 +0530

    inorder and postorder

--- test/tree/treeBasic.java
+++ test/tree/treeBasic.java
@@ -14,24 +14,61 @@ public class treeBasic {
     }
 
     static class BinaryTree {
-        static int idx = -1;
-        public static Node buildTree(int nodes[]) {
+        int idx;
+        public Node buildTree(int nodes[]) {
+            idx = -1; // reset index before building
+            return build(nodes);
+        }
+        private Node build(int nodes[]) {
             idx++;
-            if(nodes[idx] == -1){
+            if (idx >= nodes.length || nodes[idx] == -1) {
                 return null;
             }
 
             Node newNode = new Node(nodes[idx]);
-            newNode.left = buildTree(nodes);
-            newNode.right = buildTree(nodes);
-            
+            newNode.left = build(nodes);
+            newNode.right = build(nodes);
             return newNode;
         }
     }
+
+    public static void preorder(Node root){
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void inorder(Node root){
+        if(root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+    
+    public static void postorder(Node root) {
+        if(root == null) {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+
+    }
+    
     public static void main(String args[]){
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+        
+        preorder(root);
+        System.out.println(" ");
+        inorder(root);
+        System.out.println(" ");
+        postorder(root);
     }
 } 
