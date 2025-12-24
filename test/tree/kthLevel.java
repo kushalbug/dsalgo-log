a42d4d0ab7614381a962fda0ce2061cfad681bf9
kushalbug <kj814766@gmail.com>
2025-12-24 22:41:31 +0530

    Tranform to sum tree

--- test/tree/kthLevel.java
+++ test/tree/kthLevel.java
@@ -135,6 +135,31 @@ public class kthLevel {
         }
         return max + 1;
     }
+
+    public static int transform(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftChild = transform(root.left);
+        int rightChild = transform(root.right);
+
+        int data = root.data;
+        
+        int newLeft = root.left == null ? 0 : root.left.data;
+        int newRight = root.right == null ? 0 : root.right.data;
+        root.data = newLeft + leftChild + newRight + rightChild;
+
+        return data;
+    }
+
+    public static void preorder(Node root){
+        if (root == null) { 
+            return;
+        }
+        System.out.print(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
     public static void main(String[] args) {
         /* 
                  1
@@ -151,7 +176,7 @@ public class kthLevel {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        int n1 = 4, n2 = 5;
-        System.out.println(lca(root, n1, n2).data);
+        transform(root);
+        preorder(root);
     }
 }
