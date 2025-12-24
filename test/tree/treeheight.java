b9a0691f9acbe91f7e5f5d6e8a8b002d5e82b1f8
kushalbug <kj814766@gmail.com>
2025-12-24 13:13:56 +0530

    diameter of a tree O(n^2) and O(n)

--- test/tree/treeheight.java
+++ test/tree/treeheight.java
@@ -36,6 +36,41 @@ public class treeheight {
         int leftSum = sum(root.left);
         int rightSum = sum(root.right);
         return leftSum + rightSum + root.data;
+    } 
+
+    public static int diameter2(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDiameter = diameter2(root.left);
+        int rightDiameter = diameter2(root.right);
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        int rootDiameter = leftHeight + rightHeight + 1;
+        return Math.max(rootDiameter, Math.max(leftDiameter, rightDiameter));
+    }
+
+    static class Info {
+        int diameter;
+        int height;
+
+        Info(int diameter, int height) {
+            this.diameter = diameter;
+            this.height = height;
+        }
+    }
+
+    public static Info diameter(Node root) {
+        if (root == null) {
+            return new Info(0, 0);
+        }
+        Info leftInfo = diameter(root.left);
+        Info rightInfo = diameter(root.right);
+
+        int height = Math.max(leftInfo.height, rightInfo.height) + 1;
+        int diameter = Math.max(Math.max(leftInfo.diameter, rightInfo.diameter),
+                                leftInfo.height + rightInfo.height + 1);
+        return new Info(diameter, height);
     }
     public static void main(String[] args) {
         /*
@@ -53,7 +88,10 @@ public class treeheight {
         root.right.right = new Node(6);
         root.right.left = new Node(7);
 
-        int height = height(root);
-        System.out.println("Height of the tree: " + height); // Output: Height of the tree: 3
+        System.out.println("Height of tree: " + height(root));
+        System.out.println("Count of nodes: " + count(root));
+        System.out.println("Sum of nodes: " + sum(root));
+        System.out.println("Diameter of tree (O(n^2)): " + diameter2(root));
+        System.out.println("Diameter of tree (O(n)): " + diameter(root).diameter);
     }
 }
