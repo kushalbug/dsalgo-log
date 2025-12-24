d9d7f173f124ffd797c58bbb4d8bd385bec1d5ec
kushalbug <kj814766@gmail.com>
2025-12-24 17:11:13 +0530

    lowest common ancestor approach 1

--- test/tree/kthLevel.java
+++ test/tree/kthLevel.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class kthLevel {
     static class Node {
         int data;
@@ -22,6 +24,48 @@ public class kthLevel {
         Klevel(root.left, level + 1, k);
         Klevel(root.right, level + 1, k);
     }
+
+
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+        if (root == null) {
+            return false;
+        }
+        path.add(root);
+
+        if (root.data == n) {
+            return true;
+        }
+
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+
+        if (foundLeft || foundRight) {
+            return true;
+        }
+
+        path.remove(path.size() - 1);
+        return false;
+    }
+    
+    public static Node lca(Node root, int n1, int n2) {
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        getPath(root, n1, path1);
+        getPath(root, n2, path2); 
+
+        //last common ancestor
+        int i = 0;
+        for (; i < path1.size() && i < path2.size(); i++) {
+            if (path1.get(i) != path2.get(i)) {
+                break;
+            }
+        }
+
+        //last equal node -> i-1th
+        Node lca = path1.get(i - 1);
+        return lca;
+    }
     public static void main(String[] args) {
         /*
                  1
@@ -38,7 +82,7 @@ public class kthLevel {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        int k = 2;
-        Klevel(root, 1, k);
+        int n1 = 4, n2 = 5;
+        System.out.println(lca(root, n1, n2).data);
     }
 }
