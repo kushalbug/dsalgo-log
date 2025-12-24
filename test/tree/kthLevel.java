8d8d18944a27d0224e6b33ee9ad7a4efc7da6764
kushalbug <kj814766@gmail.com>
2025-12-24 21:13:45 +0530

    approach 2 for LCA

--- test/tree/kthLevel.java
+++ test/tree/kthLevel.java
@@ -66,6 +66,26 @@ public class kthLevel {
         Node lca = path1.get(i - 1);
         return lca;
     }
+
+    public static Node lca2(Node root, int n1, int n2) {
+        if (root == null) {
+            return null;
+        }
+        if (root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node leftLCA = lca2(root.left, n1, n2);
+        Node rightLCA = lca2(root.right, n1, n2);
+
+        if (leftLCA != null && rightLCA != null) {
+            return root;
+        }
+        if (leftLCA != null) {
+            return leftLCA;
+        }
+        return rightLCA;
+    }
     public static void main(String[] args) {
         /*
                  1
